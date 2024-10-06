bb5588084ade70e31207d579bdb3b61938cd44a5
alanyoungcy <50614517+alanyoungcy@users.noreply.github.com>
2024-10-06 22:34:09 +0800

    commit morth

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.loom.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.freepik.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.freepik.com',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
